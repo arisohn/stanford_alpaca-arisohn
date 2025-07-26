dab2affd6d375b9858b3cac45fcc5c2c72b262bd
arisohn <arisohn@gmail.com>
2025-07-26 16:04:43 +0900

    devel

---
+++ train_trl.sh
@@ -0,0 +1,23 @@
+CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nproc_per_node=4 --master_port=9998 train_trl.py \
+    --model_name_or_path /root/mark/llama-1/7B-hf \
+    --data_path ./alpaca_data.json \
+    --bf16 True \
+    --output_dir outputs_trl \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --eval_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True \
+    --report_to "tensorboard"
+
